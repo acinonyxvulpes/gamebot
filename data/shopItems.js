34d4d19d2845845fc5ee3e36c581c96f3810f853
cheeta <acinonyxvulpes@gmail.com>
2026-04-05 17:02:07 +0100

    Create shopItems.js

---
+++ data/shopItems.js
@@ -0,0 +1,11 @@
+export const FOOD_ITEMS = {
+    apple: { emoji: "🍎", name: "Apple", hunger: 20, cost: 10 },
+    meat: { emoji: "🍗", name: "Meat", hunger: 50, cost: 25 },
+    feast: { emoji: "🥩", name: "Feast", hunger: 100, cost: 60 }
+};
+
+export const RARE_PETS = {
+    fox: { emoji: "🦊", name: "Fox", cost: 500 },
+    panda: { emoji: "🐼", name: "Panda", cost: 1000 },
+    unicorn: { emoji: "🦄", name: "Unicorn", cost: 5000 }
+};
