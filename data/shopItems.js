f2fd8eb1347c9945372b482bcb0bc828f34156b0
cheeta <acinonyxvulpes@gmail.com>
2026-04-05 17:18:04 +0100

    Update shopItems.js

--- data/shopItems.js
+++ data/shopItems.js
@@ -9,3 +9,8 @@ export const RARE_PETS = {
     panda: { emoji: "🐼", name: "Panda", cost: 1000 },
     unicorn: { emoji: "🦄", name: "Unicorn", cost: 5000 }
 };
+export const TOY_ITEMS = {
+    ball: { emoji: "🎾", name: "Ball", reduce: 20, cost: 15 },
+    mouse: { emoji: "🧸", name: "Toy Mouse", reduce: 40, cost: 40 },
+    deluxe: { emoji: "🎁", name: "Deluxe Toy", reduce: 100, cost: 100 }
+};
