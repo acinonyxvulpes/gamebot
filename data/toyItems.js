9b384de37a6e0583b58a444db765440d97e7f3d2
cheeta <acinonyxvulpes@gmail.com>
2026-04-05 17:15:47 +0100

    Create toyItems.js

---
+++ data/toyItems.js
@@ -0,0 +1,5 @@
+export const TOY_ITEMS = {
+    ball: { emoji: "🎾", name: "Ball", reduce: 20, cost: 15 },
+    mouse: { emoji: "🧸", name: "Toy Mouse", reduce: 40, cost: 40 },
+    deluxe: { emoji: "🎁", name: "Deluxe Toy", reduce: 100, cost: 100 }
+};
